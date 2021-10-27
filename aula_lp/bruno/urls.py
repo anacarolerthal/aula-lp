daa517f4985b30a06463807fcb51da89f141b951
BrunoFornaro <brunopereirafornaro@gmail.com>
2021-10-27 09:07:10 -0300

    Adicionado o html com o render na aplicação bruno

--- aula_lp/bruno/urls.py
+++ aula_lp/bruno/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from bruno import views 
 
 urlpatterns = [
-    path("", views.index, name="index"),
+    path("", views.especial, name="especial"),
     path("index/", views.index, name="index"),
 ]
--- aula_lp/bruno/views.py
+++ aula_lp/bruno/views.py
@@ -3,3 +3,6 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("<strong>Bruno :)</strong>")
+
+def especial(request):
+    return render(request, "bruno.html")
