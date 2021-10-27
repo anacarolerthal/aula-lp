096cae76c1b0faeca8252c4e2258c58f94db4cab
Ana Carolina Erthal <81707790+anacarolerthal@users.noreply.github.com>
2021-10-27 08:49:06 -0300

    Update apps.py

--- aula_lp/carol/apps.py
+++ aula_lp/carol/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class BrunoConfig(AppConfig):
+class CarolConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'bruno'
+    name = 'carol'
