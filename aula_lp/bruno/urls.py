ddacf2288e2ad5b321fd4cc6c538a173de659ee6
BrunoFornaro <brunopereirafornaro@gmail.com>
2021-11-03 00:18:01 -0300

    Adicinoadas segunda e terceira página à aplicação bruno

--- aula_lp/bruno/urls.py
+++ aula_lp/bruno/urls.py
@@ -4,4 +4,6 @@ from bruno import views
 urlpatterns = [
     path("", views.especial, name="especial"),
     path("index/", views.index, name="index"),
+    path("pagina2/", views.pagina2, name="pagina2"),
+    path("pagina3/", views.pagina3, name="pagina3"),
 ]
--- aula_lp/bruno/views.py
+++ aula_lp/bruno/views.py
@@ -6,3 +6,9 @@ def index(request):
 
 def especial(request):
     return render(request, "bruno.html")
+
+def pagina2(request):
+    return render(request, "bruno2.html")
+
+def pagina3(request):
+    return render(request, "bruno3.html")
