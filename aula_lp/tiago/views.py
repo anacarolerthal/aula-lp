ca633dc1b720c248bba2ff5b4a9edbdd4156603b
Tiago Barradas <64672906+barrafas@users.noreply.github.com>
2021-11-03 16:33:51 -0300

    coisas da aula de hoje

--- aula_lp/tiago/views.py
+++ aula_lp/tiago/views.py
@@ -1,4 +1,21 @@
+from django.http.response import HttpResponseNotFound, HttpResponseRedirect
 from django.shortcuts import render
+from django.urls import reverse
 
 def index(request):
     return render(request, 'index.html')
+
+def GM_first(request):
+    return render(request, 'GM_first.html')
+
+def player_first(request):
+    return render(request, 'player_first.html')
+
+def dynamic_date(request, year, month):
+    if month < 0 or month > 12:
+        return HttpResponseNotFound("Página não existe!")
+    return render(request, 'index.html')
+
+def data(request, year, month):
+    redirect_url = reverse("dynamic_date", args=[year, month])
+    return HttpResponseRedirect(redirect_url)
