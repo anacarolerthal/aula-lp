28109f44a7e688da8f965b677f8f2c460466d689
BrunoFornaro <brunopereirafornaro@gmail.com>
2021-11-05 08:41:57 -0300

    Adicionadas views dinâmicas e redirecionamento à aplicação bruno

--- aula_lp/carol/urls.py
+++ aula_lp/carol/urls.py
@@ -5,6 +5,6 @@ urlpatterns = [
     path("", views.index, name="ferias1"),
     path("ferias2/", views.ferias2, name="ferias2"),
     path("ferias3/", views.ferias3, name="ferias3"),
-    path("ferias/", views.ferias, name="ferias1"),
+    path("ferias/", views.index, name="ferias1"),
     path("date/<int:month>", views.by_month, name="by_month"),
 ]
