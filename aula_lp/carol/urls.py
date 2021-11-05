4a86d77c9a429ea48ae508b2f688c1b91950539e
Ana Carolina Erthal <81707790+anacarolerthal@users.noreply.github.com>
2021-11-04 23:14:49 -0300

    meus updates aula quarta

--- aula_lp/carol/urls.py
+++ aula_lp/carol/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 from carol import views 
 
 urlpatterns = [
-    path("special/", views.special, name="special"),
-    path("index/", views.index, name="index"),
+    path("", views.index, name="ferias1"),
+    path("ferias2/", views.ferias2, name="ferias2"),
+    path("ferias3/", views.ferias3, name="ferias3"),
+    path("ferias/", views.ferias, name="ferias1"),
+    path("date/<int:month>", views.by_month, name="by_month"),
 ]
--- aula_lp/carol/views.py
+++ aula_lp/carol/views.py
@@ -2,7 +2,27 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("<strong>nao deu tempooooooooooo</strong>")
+    return HttpResponse("ferias1.html")
 
-def special(request):
-    return render(request, "ferias.html")
+def ferias2(request):
+    return render(request, "ferias2.html")
+
+def ferias3(request):
+    return render(request, "ferias3.html")
+
+def redirect_ferias(request):
+    redirect_url = reverse("index")
+    return HttpResponseRedirect(redirect_url)
+
+def by_month(request, month):
+    if month < 0 or month > 12:
+        return HttpResponseNotFound("Página não existe!")
+    elif month < 6:
+        return render(request, 'ferias2.html')
+    elif month < 8: 
+        return render(request, 'ferias1.html')
+    elif month < 10:
+        return render(request, 'ferias2.html')
+    else: 
+        return render(request, 'ferias3.html')
+
