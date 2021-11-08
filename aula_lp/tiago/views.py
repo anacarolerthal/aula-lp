b1678950cf840c0e718d867420ee54b4e02c5b54
Tiago Barradas <64672906+barrafas@users.noreply.github.com>
2021-11-08 08:41:59 -0300

    filters and tags

--- aula_lp/tiago/views.py
+++ aula_lp/tiago/views.py
@@ -3,22 +3,31 @@ from django.shortcuts import render
 from django.urls import reverse
 
 def index(request):
-    return render(request, 'index.html')
+    lista_rpgs = ["Pathfinder", "Dungeons and Dragons", "Call of Cthulu", "Dread", "Mutants and Masterminds"]
+    context = {
+        "lista_rpgs": lista_rpgs
+    }
+    return render(request, 'index.html', context)
+
 
 def GM_first(request):
     return render(request, 'GM_first.html')
 
+
 def player_first(request):
     return render(request, 'player_first.html')
+
 
 def dynamic_date(request, year, month):
     if month < 0 or month > 12:
         return HttpResponseNotFound("Página não existe!")
     return render(request, 'index.html')
 
+
 def data(request, year, month):
     redirect_url = reverse("dynamic_date", args=[year, month])
     return HttpResponseRedirect(redirect_url)
+
 
 def rpg(request, sistema):
     context = {
